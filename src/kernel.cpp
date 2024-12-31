f6c4d53c25075052211d8d8657282948bd6d63f5
Simone3838 <simone.paolini14@gmail.com>
2024-12-31 14:16:43 +0100

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -14,5 +14,5 @@ extern "C" void kernel_main() {
     std::cout << "Background color set to green!" << std::endl;
 
     // Continue with the rest of the kernel initialization
-    shell_login()
+    login()
 }
